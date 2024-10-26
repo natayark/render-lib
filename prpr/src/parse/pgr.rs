a980073586c79844ec4779d49ccc4040dfce9c2a
HLMC <hl2278535805@outlook.com>
2024-10-26 14:09:52 +0800

    Update pgr.rs

--- prpr/src/parse/pgr.rs
+++ prpr/src/parse/pgr.rs
@@ -180,7 +180,7 @@ fn parse_notes(r: f32, mut pgr: Vec<PgrNote>, speed: &mut AnimFloat, height: &mu
                 speed: if pgr.kind == 3 {
                     speed.set_time(time);
                     //pgr.speed / speed.now()
-                    pgr.speed
+                    pgr.speed / pgr.speed
                 } else {
                     pgr.speed
                 },
