9bb60922ca3b4b68b3f636fffb3b63bc37e34eb0
HLMC <hl2278535805@outlook.com>
2024-10-26 22:42:31 +0800

    Update pgr.rs

--- prpr/src/parse/pgr.rs
+++ prpr/src/parse/pgr.rs
@@ -172,8 +172,8 @@ fn parse_notes(r: f32, mut pgr: Vec<PgrNote>, speed: &mut AnimFloat, height: &mu
                         let end_time = (pgr.time + pgr.hold_time) * r;
                         height.set_time(end_time);
                         let end_height = height.now();
-                        let end_height_1 = pgr.hold_time * pgr.speed * HEIGHT_RATIO *r;
-                        NoteKind::Hold { end_time, end_height_1 }
+                        let hold_height = pgr.hold_time * pgr.speed * HEIGHT_RATIO * r;
+                        NoteKind::Hold { end_time, hold_height }
                     }
                     4 => NoteKind::Flick,
                     _ => ptl!(bail "unknown-note-type", "type" => pgr.kind),
