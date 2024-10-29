7a43713e086e650a98d43daf9c5d4c0e6d237a56
HLMC <hl2278535805@outlook.com>
2024-10-29 17:58:45 +0800

    show_below: false

--- prpr/src/parse/pgr.rs
+++ prpr/src/parse/pgr.rs
@@ -231,7 +231,7 @@ fn parse_judge_line(pgr: PgrJudgeLine, max_time: f32) -> Result<JudgeLine> {
         color: Anim::default(),
         parent: None,
         z_index: 0,
-        show_below: true,
+        show_below: false,
         attach_ui: None,
 
         cache,
