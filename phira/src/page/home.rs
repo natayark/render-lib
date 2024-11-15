fc7b77c8ae892d0b4e373a1276f3490797302eb1
HLMC <hl2278535805@outlook.com>
2024-11-15 22:56:31 +0800

    Update home.rs

--- phira/src/page/home.rs
+++ phira/src/page/home.rs
@@ -127,7 +127,7 @@ impl HomePage {
 
 impl Page for HomePage {
     fn label(&self) -> std::borrow::Cow<'static, str> {
-        "PHIRA".into()
+        "HOME".into()
     }
 
     fn enter(&mut self, s: &mut SharedState) -> Result<()> {
