f60e7dec7eaa7a2a66ab4f691a54d3b5ffabf4c3
HLMC <hl2278535805@outlook.com>
2024-12-13 17:29:14 +0800

    Update lib.rs

--- phira/src/lib.rs
+++ phira/src/lib.rs
@@ -269,7 +269,7 @@ async fn the_main() -> Result<()> {
 fn show_and_exit(msg: &str) {
     prpr::ui::Dialog::simple(msg)
         .buttons(vec!["确定".to_owned()])
-        .listener(|_, _| std::process::exit(0))
+        .listener(|_| std::process::exit(0))
         .show();
 }
 
